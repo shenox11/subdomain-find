585a09bcfd989c89ebc620a19a420e1f3214cbc2
shenox.dev <88921560+shenox11@users.noreply.github.com>
2024-12-10 02:51:43 -0700

    Update 12.py

--- tamrin/12.py
+++ tamrin/12.py
@@ -2,7 +2,7 @@ import requests
 with open ("sub.txt",'r')as subs:
     for s in subs:
         s=s.strip()
-        url=(f"https://vmp.ir/{s}")
+        url=(f"https://{s}.example.com")
         r=requests.get(url=url,timeout=5)
         if r.status_code==200:
             print(f"[valid]{url}")
